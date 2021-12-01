a28a96f9048aa66667ee73d1f4a84740f67a3bcd
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-12-01 18:44:52 -0300

    JavaScript IIFE

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,39 +1,41 @@
-const handleCreateTask = (event) => {
-  event.preventDefault();
+(() => {
+  const handleCreateTask = (event) => {
+    event.preventDefault();
 
-  const input = document.querySelector('[data-form-input]');
-  const valorInput = input.value;
-  const list = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valorInput = input.value;
+    const list = document.querySelector('[data-list]');
 
-  const task = document.createElement('li');
-  task.classList = 'task';
+    const task = document.createElement('li');
+    task.classList = 'task';
 
-  const contentTask = `<p class='content'>${valorInput}</p>`;
+    const contentTask = `<p class='content'>${valorInput}</p>`;
 
-  task.innerHTML = contentTask;
-  list.appendChild(task);
-  task.appendChild(ButtonDone());
+    task.innerHTML = contentTask;
+    list.appendChild(task);
+    task.appendChild(ButtonDone());
 
-  input.value = '';
-};
+    input.value = '';
+  };
 
-const newTask = document.querySelector('[data-form-button]');
+  const newTask = document.querySelector('[data-form-button]');
 
-newTask.addEventListener('click', handleCreateTask);
+  newTask.addEventListener('click', handleCreateTask);
 
-const ButtonDone = () => {
-  const buttonDone = document.createElement('button');
-  buttonDone.innerText = 'Done';
-  buttonDone.classList = 'check-button';
+  const ButtonDone = () => {
+    const buttonDone = document.createElement('button');
+    buttonDone.innerText = 'Done';
+    buttonDone.classList = 'check-button';
 
-  buttonDone.addEventListener('click', doneTask);
+    buttonDone.addEventListener('click', doneTask);
 
-  return buttonDone;
-};
+    return buttonDone;
+  };
 
-const doneTask = (event) => {
-  const buttonDone = event.target;
-  const task = buttonDone.parentElement;
+  const doneTask = (event) => {
+    const buttonDone = event.target;
+    const task = buttonDone.parentElement;
 
-  task.classList.toggle('done');
-};
+    task.classList.toggle('done');
+  };
+})();
