1f305895d523fbe393e7397975d25a8961874216
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-12-01 14:41:41 -0300

    Form behavior

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,7 +1,12 @@
-const novaTarefa = document.querySelector('[data-form-button]');
+const handleCreateTask = (event) => {
+  event.preventDefault();
 
-novaTarefa.addEventListener('click', () => {
   const input = document.querySelector('[data-form-input]');
   const valorInput = input.value;
   console.log(valorInput);
-});
+  input.value = '';
+};
+
+const newTask = document.querySelector('[data-form-button]');
+
+newTask.addEventListener('click', handleCreateTask);
